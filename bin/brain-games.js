20cfffba583ceb96b8e1d69134752ed1d009708f
Maksim Shpagin <delf1008@mail.ru>
2024-12-12 09:07:37 +1000

    do step three

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,4 @@
-console.log('Welcome to the Brain Games!');
+import { greetings } from "../src/cli.js";
+
+console.log("Welcome to the Brain Games!");
+greetings();
---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readLineSync from "readline-sync";
+
+const greetings = () => {
+  const userName = readLineSync.question("May I have your name? ");
+  console.log(`Hello, ${userName}!`);
+};
+
+export { greetings };
